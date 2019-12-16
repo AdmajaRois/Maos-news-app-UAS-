e15f7ab061028c954aeff3d637a3b31fa635cee9
AdmajaRois <rois.da.silva@gmail.com>
2019-12-16 22:02:42 +0700

    viewpager berhasil dibuat

--- app/src/main/java/com/admaja/maos_aplikasiberita/activity/WelcomeActivity.java
+++ app/src/main/java/com/admaja/maos_aplikasiberita/activity/WelcomeActivity.java
@@ -3,17 +3,19 @@ package com.admaja.maos_aplikasiberita.activity;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.PagerAdapter;
-import androidx.viewpager.widget.PagerTabStrip;
 import androidx.viewpager.widget.ViewPager;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -23,8 +25,9 @@ import com.admaja.maos_aplikasiberita.R;
 import com.admaja.maos_aplikasiberita.adapter.PrefManager;
 
 public class WelcomeActivity extends AppCompatActivity {
+
     private ViewPager viewPager;
-    private MyViewPagerAdapter  myViewPagerAdapter;
+    private MyViewPagerAdapter myViewPagerAdapter;
     private LinearLayout dotsLayout;
     private TextView[] dots;
     private int[] layouts;
@@ -34,27 +37,33 @@ public class WelcomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         prefManager = new PrefManager(this);
         if (!prefManager.isFirstTimeLaunch()){
             launchHomeScreen();
             finish();
         }
-        if (Build.VERSION.SDK_INT >= 21){
-            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_FULLSCREEN);
-        }
 
+        if (Build.VERSION.SDK_INT >=21){
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_FULLSCREEN);
+
+        }
         setContentView(R.layout.activity_welcome);
 
         viewPager = findViewById(R.id.view_pager);
         dotsLayout = findViewById(R.id.layoutDots);
-        btnSkip = findViewById(R.id.btn_skip);
         btnNext = findViewById(R.id.btn_next);
+        btnSkip = findViewById(R.id.btn_skip);
 
         layouts = new int[]{
                 R.layout.welcome_slide1,
                 R.layout.welcome_slide2,
-                R.layout.welcome_slide3};
+                R.layout.welcome_slide3
+        };
+
         addBottomDots(0);
+
+
 
         myViewPagerAdapter = new MyViewPagerAdapter();
         viewPager.setAdapter(myViewPagerAdapter);
@@ -79,58 +88,62 @@ public class WelcomeActivity extends AppCompatActivity {
             }
         });
     }
-    private void addBottomDots(int currentPage){
+
+    private void addBottomDots(int currentPage) {
         dots = new TextView[layouts.length];
 
         int[] colorsActive = getResources().getIntArray(R.array.array_dot_active);
-        int[] colorInactive = getResources().getIntArray(R.array.array_dot_inactive);
+        int[] colorsInactive = getResources().getIntArray(R.array.array_dot_inactive);
 
         dotsLayout.removeAllViews();
         for (int i = 0; i < dots.length; i++){
             dots[i] = new TextView(this);
             dots[i].setText(Html.fromHtml("&#8226"));
             dots[i].setTextSize(35);
-            dots[i].setTextColor(colorInactive[currentPage]);
+            dots[i].setTextColor(colorsInactive[currentPage]);
             dotsLayout.addView(dots[i]);
         }
-        if (dots.length>0)
+
+        if (dots.length > 0)
             dots[currentPage].setTextColor(colorsActive[currentPage]);
     }
+
     private int getItem(int i){
-        return viewPager.getCurrentItem()+1;
+        return viewPager.getCurrentItem()+i;
     }
 
-    private void launchHomeScreen(){
+    private void launchHomeScreen() {
+        prefManager.setFirstTimeLaunch(false);
         startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
         finish();
     }
 
-  ViewPager.OnPageChangeListener viewPagerPageChangeListener = new ViewPager.OnPageChangeListener() {
-      @Override
-      public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+    ViewPager.OnPageChangeListener viewPagerPageChangeListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageSelected(int position) {
+            addBottomDots(position);
 
-      }
-
-      @Override
-      public void onPageSelected(int position) {
-          addBottomDots(position);
-
-            if (position == layouts.length -1){
+            if (position == layouts.length-1){
                 btnNext.setText(getString(R.string.start));
                 btnSkip.setVisibility(View.GONE);
             }else {
                 btnNext.setText(getString(R.string.next));
                 btnSkip.setVisibility(View.VISIBLE);
             }
-      }
+        }
 
-      @Override
-      public void onPageScrollStateChanged(int state) {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 
-      }
-  };
+        }
 
-    public class MyViewPagerAdapter extends PagerAdapter {
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
+
+    private class MyViewPagerAdapter extends PagerAdapter {
         private LayoutInflater layoutInflater;
 
         public MyViewPagerAdapter() {
@@ -140,8 +153,10 @@ public class WelcomeActivity extends AppCompatActivity {
         @Override
         public Object instantiateItem(@NonNull ViewGroup container, int position) {
             layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
             View view = layoutInflater.inflate(layouts[position], container, false);
             container.addView(view);
+
             return view;
         }
 
@@ -151,15 +166,14 @@ public class WelcomeActivity extends AppCompatActivity {
         }
 
         @Override
-        public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        public boolean isViewFromObject( View view, Object object) {
             return view == object;
         }
 
         @Override
         public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
-            View view = (View) object;
-            container.removeView(view);
+           View view = (View) object;
+           container.removeView(view);
         }
     }
-
 }
