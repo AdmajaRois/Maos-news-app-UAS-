8aba6f9ebaa74f09d09cb4c23d5890839371e95c
AdmajaRois <rois.da.silva@gmail.com>
2019-12-19 00:07:21 +0700

    menambahkan class EmptyRecycleView, class ini merupakan subclass dari RecycleView untuk memperlihatkan data ada atau tidak

--- app/src/main/java/com/admaja/maos_aplikasiberita/adapter/NewsAdapter.java
+++ app/src/main/java/com/admaja/maos_aplikasiberita/adapter/NewsAdapter.java
@@ -1,15 +1,54 @@
 package com.admaja.maos_aplikasiberita.adapter;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.admaja.maos_aplikasiberita.News;
+import com.admaja.maos_aplikasiberita.R;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
 public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
+    private Context mContext;
+    private List<News> mNewsList;
+    private SharedPreferences sharePref;
+
+    /**
+     * Constructs a new {@link NewsAdapter}
+     * @param context of the app
+     * @param newsList is the list of news, which is the data source of the adapter
+     */
+
+    public NewsAdapter(Context mContext, List<News> mNewsList) {
+        mContext = mContext;
+        mNewsList = mNewsList;
+    }
+
     @NonNull
     @Override
     public NewsAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.news_card_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mNewsList.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
     }
 
     @Override
@@ -17,8 +56,4 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
 
     }
 
-    @Override
-    public int getItemCount() {
-        return 0;
-    }
 }
