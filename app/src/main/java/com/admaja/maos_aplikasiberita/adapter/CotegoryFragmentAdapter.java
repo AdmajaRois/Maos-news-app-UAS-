3d582f4bd83855a129d7905fd14fb0b236b33fe1
AdmajaRois <rois.da.silva@gmail.com>
2019-12-18 22:31:06 +0700

    menambahkan atribut context pada CotegoryFragmentAdapter

--- app/src/main/java/com/admaja/maos_aplikasiberita/adapter/CotegoryFragmentAdapter.java
+++ app/src/main/java/com/admaja/maos_aplikasiberita/adapter/CotegoryFragmentAdapter.java
@@ -1,11 +1,15 @@
 package com.admaja.maos_aplikasiberita.adapter;
 
+import android.content.Context;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
 public class CotegoryFragmentAdapter extends FragmentPagerAdapter {
+
+    private Context mContext;
 
     public CotegoryFragmentAdapter(@NonNull FragmentManager fm) {
         super(fm);
