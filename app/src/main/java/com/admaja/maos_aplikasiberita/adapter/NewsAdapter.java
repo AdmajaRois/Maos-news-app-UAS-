aed3292d8bc892c6a3b7be2a5043bbd22669e218
AdmajaRois <rois.da.silva@gmail.com>
2019-12-18 23:17:02 +0700

    menambahkan class NewsAdapter untuk menampilkan berita

---
+++ app/src/main/java/com/admaja/maos_aplikasiberita/adapter/NewsAdapter.java
@@ -0,0 +1,24 @@
+package com.admaja.maos_aplikasiberita.adapter;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
+    @NonNull
+    @Override
+    public NewsAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NewsAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
