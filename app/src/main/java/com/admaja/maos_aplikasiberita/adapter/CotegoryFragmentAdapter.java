95b366e0bf3ceff15e7ab96651683f4bb506078d
AdmajaRois <rois.da.silva@gmail.com>
2019-12-18 22:19:36 +0700

    menambahkan extends fragmentadapter ke classs CategoryFragmentAdapter dan melakukan implementasi method

--- app/src/main/java/com/admaja/maos_aplikasiberita/adapter/CotegoryFragmentAdapter.java
+++ app/src/main/java/com/admaja/maos_aplikasiberita/adapter/CotegoryFragmentAdapter.java
@@ -1,5 +1,24 @@
 package com.admaja.maos_aplikasiberita.adapter;
 
-public class CotegoryFragmentAdapter {
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
 
+public class CotegoryFragmentAdapter extends FragmentPagerAdapter {
+
+    public CotegoryFragmentAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
 }
