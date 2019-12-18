e50048fd9c000648fc482310fa983fffe04332ff
AdmajaRois <rois.da.silva@gmail.com>
2019-12-19 00:12:37 +0700

    menambahkan fungsi checkIfEmpty() untuk menyembunyikan atau  menampilkan data

--- app/src/main/java/com/admaja/maos_aplikasiberita/EmptyRecyclerView.java
+++ app/src/main/java/com/admaja/maos_aplikasiberita/EmptyRecyclerView.java
@@ -1,5 +1,45 @@
 package com.admaja.maos_aplikasiberita;
 
-public class EmptyRecyclerView {
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class EmptyRecyclerView extends RecyclerView {
+    private View mEmptyView;
+
+    final private AdapterDataObserver observer = new AdapterDataObserver() {
+        @Override
+        public void onChanged() {
+            checkIfEmpty();
+        }
+
+        @Override
+        public void onItemRangeInserted(int positionStart, int itemCount) {
+           checkIfEmpty();
+        }
+
+        @Override
+        public void onItemRangeRemoved(int positionStart, int itemCount) {
+           checkIfEmpty();
+        }
+    }
+    public EmptyRecyclerView(@NonNull Context context) {
+        super(context);
+    }
+
+    public EmptyRecyclerView(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public EmptyRecyclerView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    private void checkIfEmpty() {
+
+    }
 }
