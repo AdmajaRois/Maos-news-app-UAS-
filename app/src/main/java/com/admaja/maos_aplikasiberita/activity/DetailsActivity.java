35a81b0f2d80190c713713ea3aab9cc908696346
AdmajaRois <rois.da.silva@gmail.com>
2019-12-19 21:40:44 +0700

    membuat atribut WebView, ProgresBar, url pada DetailActivity

--- app/src/main/java/com/admaja/maos_aplikasiberita/activity/DetailsActivity.java
+++ app/src/main/java/com/admaja/maos_aplikasiberita/activity/DetailsActivity.java
@@ -2,15 +2,23 @@ package com.admaja.maos_aplikasiberita.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.widget.ProgressBar;
 
 import com.admaja.maos_aplikasiberita.R;
 
 public class DetailsActivity extends AppCompatActivity {
+    WebView webView;
+    ProgressBar loader;
+    String url = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+
     }
 }
